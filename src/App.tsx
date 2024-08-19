2bbaaa2f684669301e9582ee68d3aaadc1e91d42
Maciej Kaczanowski <maciej.emil.kaczanowski@gmail.com>
2024-08-20 01:33:05 +0200

    feat(apiData): display weather data

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,64 @@ export interface CityDataProps {
     lat: number
 }
 
+export interface WeatherDataProps {
+    "coord": {
+        "lon": number;
+        "lat": number
+    };
+    "weather": [
+        {
+            "id": number;
+            "main": string;
+            "description": string;
+            "icon": string;
+        }
+    ];
+    "base": string;
+    "main": {
+        "temp": number;
+        "feels_like": number;
+        "temp_min": number;
+        "temp_max": number;
+        "pressure": number;
+        "humidity": number;
+        "sea_level": number;
+        "grnd_level": number;
+    };
+    rain: {
+       "1h": number;
+       "3h": number;
+    };
+    snow: {
+       "1h": number;
+       "3h": number;
+    };
+    cloud: {
+      all: number;
+    };
+    "visibility": number;
+    "wind": {
+        "speed": number;
+        "deg": number;
+        "gust": number;
+    };
+    "clouds": {
+        "all": number;
+    };
+    "dt": number;
+    "sys": {
+        "type": number;
+        "id": number;
+        "country": string;
+        "sunrise": number;
+        "sunset": number;
+    };
+    "timezone": number;
+    "id": number;
+    "name": string;
+    "cod": number;
+}
+
 export async function fetchCityData(cityName: string): Promise<
     Array<CityDataProps>
 > {
@@ -19,11 +77,9 @@ export async function fetchCityData(cityName: string): Promise<
     return await response.json()
 }
 
-export async function fetchWeatherData(cityData: CityDataProps): Promise<
-    Array<{}>
-> {
+export async function fetchWeatherData(cityData: CityDataProps): Promise<WeatherDataProps> {
     const response = await fetch(
-        `${process.env.REACT_APP_WERATHER_API_URL}lat=${cityData.lat}&lon=${cityData.lon}&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`,
+        `${process.env.REACT_APP_WERATHER_API_URL}lat=${cityData.lat}&lon=${cityData.lon}&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}&units=metric`,
     )
 
     return await response.json()
@@ -37,16 +93,16 @@ function App() {
 
     const {data: weatherData, mutateAsync: fetchWeatherDataMutation} = useMutation({
         mutationFn: fetchWeatherData,
-        onSuccess: (cityData) => {
-            if (cityData.length > 0) {
-                saverRecentSearch(cityData[0] as CityDataProps)
+        onSuccess: (weatherData: WeatherDataProps) => {
+            if (weatherData) {
+                saverRecentSearch({name: weatherData.name, ...weatherData.coord})
             }
-            queryClient.invalidateQueries({queryKey: ["citydata"]})
+            queryClient.invalidateQueries({queryKey: ["weatherdata"]})
         }
     })
 
 
-    const {data: cityData, mutateAsync: fetchCityDataMutation} = useMutation({
+    const {mutateAsync: fetchCityDataMutation} = useMutation({
         mutationFn: fetchCityData,
         onSuccess: async (cityData) => {
             if (cityData.length > 0) {
@@ -58,6 +114,7 @@ function App() {
             await queryClient.invalidateQueries({queryKey: ["citydata"]})
         },
     })
+
 
     return (
         <div className="flex flex-col items-center">
@@ -75,9 +132,15 @@ function App() {
                         Search
                     </button>
                 </div>
-                <div className="pt-6 pb-6">
-                    data
-                </div>
+                {weatherData && <div className="pt-6 pb-6">
+                    <p>Feels like temperature: {weatherData.main.feels_like}C</p>
+                    <p>Temperature: {weatherData.main.temp}C</p>
+                    <p>Minimal temperature: {weatherData.main.temp_min}C</p>
+                    <p>Maximal temperature: {weatherData.main.temp_max}C</p>
+                    <p>Atmospheric pressure: {weatherData.main.grnd_level}hPa</p>
+                    <p>Humidity: {weatherData.main.humidity}%</p>
+                    <p>Humidity: {weatherData.main.humidity}%</p>
+                </div>}
                 <div className="flex flex-col items-center">
                     <h2 className="pb-2">Recent Search</h2>
                     <div className="flex flex-col-reverse items-center">
